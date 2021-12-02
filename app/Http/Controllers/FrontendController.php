3a9fcef812e76ef54c2a89c1fe49edeb6452e56f
Arpit Vijay Vergia <arpitvijay0910@gmail.com>
2021-12-02 10:08:55 +0530

    Updated: Controller

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -27,6 +27,22 @@ class FrontendController extends Controller
     {
         return view('frontend.cart');
     }
+    public function checkout()
+    {
+        return view('frontend.checkout');
+    }
+    public function profile()
+    {
+        return view('frontend.profile');
+    }
+    public function aboutUs()
+    {
+        return view('frontend.about-us');
+    }
+    public function orderTracking()
+    {
+        return view('frontend.order-tracking');
+    }
     public function contact()
     {
         return view('frontend.contact');
